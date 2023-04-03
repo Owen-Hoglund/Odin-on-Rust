670f8a6df9f6eac3b78e70aadad52b9e38a67baa
Owen-Hoglund <owenhoglund@gmail.com>
2023-04-03 00:21:32 -0500

    Added the Rust package

---
+++ rusty-odin/target/debug/deps/rusty_odin-aeb3f8613afd582a.d
@@ -0,0 +1,5 @@
+/home/owen/Documents/Code/Projects/Odin-on-Rust/rusty-odin/target/debug/deps/rusty_odin-aeb3f8613afd582a: src/main.rs
+
+/home/owen/Documents/Code/Projects/Odin-on-Rust/rusty-odin/target/debug/deps/rusty_odin-aeb3f8613afd582a.d: src/main.rs
+
+src/main.rs:
---
+++ rusty-odin/target/debug/rusty-odin.d
@@ -0,0 +1,1 @@
+/home/owen/Documents/Code/Projects/Odin-on-Rust/rusty-odin/target/debug/rusty-odin: /home/owen/Documents/Code/Projects/Odin-on-Rust/rusty-odin/src/main.rs
